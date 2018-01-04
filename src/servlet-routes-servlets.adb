e5d9daf98981b7d03b46e0e3411abaaf28d19bbd
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-04 09:25:13 +0100

    Rename package ASF into Servlet

--- src/servlet-routes-servlets.adb
+++ src/servlet-routes-servlets.adb
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------
---  asf-routes -- Request routing
+--  servlet-routes -- Request routing
 --  Copyright (C) 2015 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
@@ -17,19 +17,19 @@
 -----------------------------------------------------------------------
 with Ada.Unchecked_Deallocation;
 
-package body ASF.Routes.Servlets is
+package body Servlet.Routes.Servlets is
 
-   use type ASF.Filters.Filter_Access;
-   use type ASF.Filters.Filter_List_Access;
+   use type Servlet.Filters.Filter_Access;
+   use type Servlet.Filters.Filter_List_Access;
 
    procedure Free is
-     new Ada.Unchecked_Deallocation (Object => ASF.Filters.Filter_List,
-                                     Name   => ASF.Filters.Filter_List_Access);
+     new Ada.Unchecked_Deallocation (Object => Servlet.Filters.Filter_List,
+                                     Name   => Servlet.Filters.Filter_List_Access);
 
    --  ------------------------------
    --  Get the servlet to call for the route.
    --  ------------------------------
-   function Get_Servlet (Route : in Servlet_Route_Type) return ASF.Servlets.Servlet_Access is
+   function Get_Servlet (Route : in Servlet_Route_Type) return Servlet.Servlets.Servlet_Access is
    begin
       return Route.Servlet;
    end Get_Servlet;
@@ -38,14 +38,14 @@ package body ASF.Routes.Servlets is
    --  Append the filter to the filter list defined by the mapping node.
    --  ------------------------------
    procedure Append_Filter (Route  : in out Servlet_Route_Type;
-                            Filter : in ASF.Filters.Filter_Access) is
-      List : ASF.Filters.Filter_List_Access;
+                            Filter : in Servlet.Filters.Filter_Access) is
+      List : Servlet.Filters.Filter_List_Access;
    begin
       --  Filters are executed through the <b>Filter_Chain.Do_Filter</b> method
       --  starting from the last position to the first.  To append a filter,
       --  it must be inserted as first position of the list.
       if Route.Filters = null then
-         List := new ASF.Filters.Filter_List (1 .. 1);
+         List := new Servlet.Filters.Filter_List (1 .. 1);
       else
          --  Check that the filter is not already executed.
          for I in Route.Filters'Range loop
@@ -53,7 +53,7 @@ package body ASF.Routes.Servlets is
                return;
             end if;
          end loop;
-         List := new ASF.Filters.Filter_List (1 .. Route.Filters'Last + 1);
+         List := new Servlet.Filters.Filter_List (1 .. Route.Filters'Last + 1);
          List (2 .. List'Last) := Route.Filters.all;
          Free (Route.Filters);
       end if;
@@ -74,7 +74,7 @@ package body ASF.Routes.Servlets is
    --  Get the servlet to call for the route.
    --  ------------------------------
    overriding
-   function Get_Servlet (Route : in Proxy_Route_Type) return ASF.Servlets.Servlet_Access is
+   function Get_Servlet (Route : in Proxy_Route_Type) return Servlet.Servlets.Servlet_Access is
    begin
       if Route.Route /= null then
          return Route.Route.Get_Servlet;
@@ -83,4 +83,4 @@ package body ASF.Routes.Servlets is
       end if;
    end Get_Servlet;
 
-end ASF.Routes.Servlets;
+end Servlet.Routes.Servlets;
