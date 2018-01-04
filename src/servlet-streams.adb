c1fc494ec76499434031a03d1ebb6fbfb7dbefea
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-04 11:20:11 +0100

    Rename ASF into Servlet

--- src/servlet-streams.adb
+++ src/servlet-streams.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  Servlet.Streams -- Print streams for servlets
---  Copyright (C) 2010, 2011, 2012, 2013 Stephane Carrez
+--  Copyright (C) 2010, 2011, 2012, 2013, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
