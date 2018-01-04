75dbd8215a0c518e0f57645cc3928a52d0cee170
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-04 09:37:08 +0100

    Rename package ASF into Servlet

--- regtests/servlet-rest-tests.adb
+++ regtests/servlet-rest-tests.adb
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------
---  asf-rest-tests - Unit tests for ASF.Rest and ASF.Servlets.Rest
+--  servlet-rest-tests - Unit tests for Servlet.Rest and Servlet.Servlets.Rest
 --  Copyright (C) 2016, 2017 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
@@ -23,13 +23,13 @@ with Util.Measures;
 with EL.Contexts.Default;
 
 with Security.Permissions;
-with ASF.Requests.Mockup;
-with ASF.Responses.Mockup;
-with ASF.Servlets.Rest;
-with ASF.Rest.Definition;
-with ASF.Rest.Operation;
+with Servlet.Requests.Mockup;
+with Servlet.Responses.Mockup;
+with Servlet.Servlets.Rest;
+with Servlet.Rest.Definition;
+with Servlet.Rest.Operation;
 
-package body ASF.Rest.Tests is
+package body Servlet.Rest.Tests is
 
    package Caller is new Util.Test_Caller (Test, "Rest");
 
@@ -37,127 +37,127 @@ package body ASF.Rest.Tests is
       new Security.Permissions.Definition ("test-permission");
 
    package API_Simple_Get is
-     new ASF.Rest.Operation (Handler    => Simple_Get'Access,
+     new Servlet.Rest.Operation (Handler    => Simple_Get'Access,
                              URI        => "/simple/:id");
 
    package API_Simple_List is
-     new ASF.Rest.Operation (Handler    => Simple_Get'Access,
+     new Servlet.Rest.Operation (Handler    => Simple_Get'Access,
                              URI        => "/simple");
 
    package API_Simple_Post is
-     new ASF.Rest.Operation (Handler    => Simple_Post'Access,
+     new Servlet.Rest.Operation (Handler    => Simple_Post'Access,
                              URI        => "/simple",
-                             Method     => ASF.Rest.POST);
+                             Method     => Servlet.Rest.POST);
 
    package API_Simple_Delete is
-     new ASF.Rest.Operation (Handler    => Simple_Delete'Access,
+     new Servlet.Rest.Operation (Handler    => Simple_Delete'Access,
                              URI        => "/simple/:id",
-                             Method     => ASF.Rest.DELETE);
+                             Method     => Servlet.Rest.DELETE);
 
    package API_Simple_Put is
-     new ASF.Rest.Operation (Handler    => Simple_Put'Access,
+     new Servlet.Rest.Operation (Handler    => Simple_Put'Access,
                              URI        => "/simple/:id",
-                             Method     => ASF.Rest.PUT);
+                             Method     => Servlet.Rest.PUT);
 
    package Test_API_Definition is
-     new ASF.Rest.Definition (Object_Type => Test_API,
+     new Servlet.Rest.Definition (Object_Type => Test_API,
                               URI         => "/test");
 
    package API_Create is
      new Test_API_Definition.Definition (Handler    => Create'Access,
-                                         Method     => ASF.Rest.POST,
+                                         Method     => Servlet.Rest.POST,
                                          Pattern    => "",
                                          Permission => Test_Permission.Permission);
 
    package API_Update is
      new Test_API_Definition.Definition (Handler    => Update'Access,
-                                         Method     => ASF.Rest.PUT,
+                                         Method     => Servlet.Rest.PUT,
                                          Pattern    => ":id",
                                          Permission => 0);
 
    package API_Delete is
      new Test_API_Definition.Definition (Handler    => Delete'Access,
-                                         Method     => ASF.Rest.DELETE,
+                                         Method     => Servlet.Rest.DELETE,
                                          Pattern    => ":id",
                                          Permission => 0);
 
    package API_List is
      new Test_API_Definition.Definition (Handler    => List'Access,
-                                         Method     => ASF.Rest.GET,
+                                         Method     => Servlet.Rest.GET,
                                          Pattern    => "",
                                          Permission => 0);
 
    package API_Get is
      new Test_API_Definition.Definition (Handler    => List'Access,
-                                         Method     => ASF.Rest.GET,
+                                         Method     => Servlet.Rest.GET,
                                          Pattern    => ":id",
                                          Permission => 0);
 
-   procedure Simple_Get (Req    : in out ASF.Rest.Request'Class;
-                         Reply  : in out ASF.Rest.Response'Class;
-                         Stream : in out ASF.Rest.Output_Stream'Class) is
+   procedure Simple_Get (Req    : in out Servlet.Rest.Request'Class;
+                         Reply  : in out Servlet.Rest.Response'Class;
+                         Stream : in out Servlet.Rest.Output_Stream'Class) is
       Data : Test_API;
    begin
       List (Data, Req, Reply, Stream);
    end Simple_Get;
 
-   procedure Simple_Put (Req    : in out ASF.Rest.Request'Class;
-                         Reply  : in out ASF.Rest.Response'Class;
-                         Stream : in out ASF.Rest.Output_Stream'Class) is
+   procedure Simple_Put (Req    : in out Servlet.Rest.Request'Class;
+                         Reply  : in out Servlet.Rest.Response'Class;
+                         Stream : in out Servlet.Rest.Output_Stream'Class) is
       Data : Test_API;
    begin
       Update (Data, Req, Reply, Stream);
    end Simple_Put;
 
-   procedure Simple_Post (Req    : in out ASF.Rest.Request'Class;
-                          Reply  : in out ASF.Rest.Response'Class;
-                          Stream : in out ASF.Rest.Output_Stream'Class) is
+   procedure Simple_Post (Req    : in out Servlet.Rest.Request'Class;
+                          Reply  : in out Servlet.Rest.Response'Class;
+                          Stream : in out Servlet.Rest.Output_Stream'Class) is
       Data : Test_API;
    begin
       Create (Data, Req, Reply, Stream);
    end Simple_Post;
 
-   procedure Simple_Delete (Req    : in out ASF.Rest.Request'Class;
-                            Reply  : in out ASF.Rest.Response'Class;
-                            Stream : in out ASF.Rest.Output_Stream'Class) is
+   procedure Simple_Delete (Req    : in out Servlet.Rest.Request'Class;
+                            Reply  : in out Servlet.Rest.Response'Class;
+                            Stream : in out Servlet.Rest.Output_Stream'Class) is
       Data : Test_API;
    begin
       Delete (Data, Req, Reply, Stream);
    end Simple_Delete;
 
    procedure Create (Data   : in out Test_API;
-                     Req    : in out ASF.Rest.Request'Class;
-                     Reply  : in out ASF.Rest.Response'Class;
-                     Stream : in out ASF.Rest.Output_Stream'Class) is
+                     Req    : in out Servlet.Rest.Request'Class;
+                     Reply  : in out Servlet.Rest.Response'Class;
+                     Stream : in out Servlet.Rest.Output_Stream'Class) is
    begin
-      Reply.Set_Status (ASF.Responses.SC_CREATED);
+      Reply.Set_Status (Servlet.Responses.SC_CREATED);
 
-      --  ASF.Rest.Created (Reply, "23");
+      --  Servlet.Rest.Created (Reply, "23");
       Reply.Set_Header (Name  => "Location",
                         Value => "/test/23");
    end Create;
 
    procedure Update (Data   : in out Test_API;
-                     Req    : in out ASF.Rest.Request'Class;
-                     Reply  : in out ASF.Rest.Response'Class;
-                     Stream : in out ASF.Rest.Output_Stream'Class) is
+                     Req    : in out Servlet.Rest.Request'Class;
+                     Reply  : in out Servlet.Rest.Response'Class;
+                     Stream : in out Servlet.Rest.Output_Stream'Class) is
       Id : constant String := Req.Get_Path_Parameter (1);
    begin
-      Reply.Set_Status (ASF.Responses.SC_OK);
+      Reply.Set_Status (Servlet.Responses.SC_OK);
    end Update;
 
    procedure Delete (Data   : in out Test_API;
-                     Req    : in out ASF.Rest.Request'Class;
-                     Reply  : in out ASF.Rest.Response'Class;
-                     Stream : in out ASF.Rest.Output_Stream'Class) is
+                     Req    : in out Servlet.Rest.Request'Class;
+                     Reply  : in out Servlet.Rest.Response'Class;
+                     Stream : in out Servlet.Rest.Output_Stream'Class) is
    begin
-      Reply.Set_Status (ASF.Responses.SC_NO_CONTENT);
+      Reply.Set_Status (Servlet.Responses.SC_NO_CONTENT);
    end Delete;
 
    procedure List (Data   : in out Test_API;
-                   Req    : in out ASF.Rest.Request'Class;
-                   Reply  : in out ASF.Rest.Response'Class;
-                   Stream : in out ASF.Rest.Output_Stream'Class) is
+                   Req    : in out Servlet.Rest.Request'Class;
+                   Reply  : in out Servlet.Rest.Response'Class;
+                   Stream : in out Servlet.Rest.Output_Stream'Class) is
    begin
       if Req.Get_Path_Parameter_Count = 0 then
          Stream.Start_Document;
@@ -175,9 +175,9 @@ package body ASF.Rest.Tests is
             Id : constant String := Req.Get_Path_Parameter (1);
          begin
             if Id = "100" then
-               Reply.Set_Status (ASF.Responses.SC_NOT_FOUND);
+               Reply.Set_Status (Servlet.Responses.SC_NOT_FOUND);
             elsif Id /= "44" then
-               Reply.Set_Status (ASF.Responses.SC_GONE);
+               Reply.Set_Status (Servlet.Responses.SC_GONE);
             end if;
          end;
       end if;
@@ -185,19 +185,19 @@ package body ASF.Rest.Tests is
 
    procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite) is
    begin
-      Caller.Add_Test (Suite, "Test ASF.Rest.POST API operation",
+      Caller.Add_Test (Suite, "Test Servlet.Rest.POST API operation",
                        Test_Create'Access);
-      Caller.Add_Test (Suite, "Test ASF.Rest.GET API operation",
+      Caller.Add_Test (Suite, "Test Servlet.Rest.GET API operation",
                        Test_Get'Access);
-      Caller.Add_Test (Suite, "Test ASF.Rest.PUT API operation",
+      Caller.Add_Test (Suite, "Test Servlet.Rest.PUT API operation",
                        Test_Update'Access);
-      Caller.Add_Test (Suite, "Test ASF.Rest.DELETE API operation",
+      Caller.Add_Test (Suite, "Test Servlet.Rest.DELETE API operation",
                        Test_Delete'Access);
-      Caller.Add_Test (Suite, "Test ASF.Rest.TRACE API operation",
+      Caller.Add_Test (Suite, "Test Servlet.Rest.TRACE API operation",
                        Test_Invalid'Access);
    end Add_Tests;
 
-   procedure Benchmark (Ctx    : in ASF.Servlets.Servlet_Registry;
+   procedure Benchmark (Ctx    : in Servlet.Servlets.Servlet_Registry;
                         Title  : in String;
                         Method : in String;
                         URI    : in String) is
@@ -205,15 +205,15 @@ package body ASF.Rest.Tests is
    begin
       for I in 1 .. 1000 loop
          declare
-            Request : ASF.Requests.Mockup.Request;
-            Reply   : ASF.Responses.Mockup.Response;
-            Dispatcher : constant ASF.Servlets.Request_Dispatcher
+            Request : Servlet.Requests.Mockup.Request;
+            Reply   : Servlet.Responses.Mockup.Response;
+            Dispatcher : constant Servlet.Servlets.Request_Dispatcher
               := Ctx.Get_Request_Dispatcher (Path => URI);
             Result : Ada.Strings.Unbounded.Unbounded_String;
          begin
             Request.Set_Method (Method);
             Request.Set_Request_URI (URI);
-            ASF.Servlets.Forward (Dispatcher, Request, Reply);
+            Servlet.Servlets.Forward (Dispatcher, Request, Reply);
          end;
       end loop;
 
@@ -224,24 +224,24 @@ package body ASF.Rest.Tests is
                              Method : in String;
                              URI    : in String;
                              Status : in Natural) is
-      use ASF.Servlets;
+      use Servlet.Servlets;
       use Util.Tests;
 
       Ctx     : Servlet_Registry;
-      S1      : aliased ASF.Servlets.Rest.Rest_Servlet;
+      S1      : aliased Servlet.Servlets.Rest.Rest_Servlet;
       EL_Ctx  : EL.Contexts.Default.Default_Context;
-      Request : ASF.Requests.Mockup.Request;
-      Reply   : ASF.Responses.Mockup.Response;
+      Request : Servlet.Requests.Mockup.Request;
+      Reply   : Servlet.Responses.Mockup.Response;
    begin
       Ctx.Add_Servlet ("API", S1'Unchecked_Access);
       Ctx.Add_Mapping (Name => "API", Pattern => "/simple/*");
       Ctx.Start;
       Ctx.Dump_Routes (Util.Log.INFO_LEVEL);
-      ASF.Rest.Register (Ctx, API_Simple_Get.Definition);
-      ASF.Rest.Register (Ctx, API_Simple_List.Definition);
-      ASF.Rest.Register (Ctx, API_Simple_Post.Definition);
-      ASF.Rest.Register (Ctx, API_Simple_Put.Definition);
-      ASF.Rest.Register (Ctx, API_Simple_Delete.Definition);
+      Servlet.Rest.Register (Ctx, API_Simple_Get.Definition);
+      Servlet.Rest.Register (Ctx, API_Simple_List.Definition);
+      Servlet.Rest.Register (Ctx, API_Simple_Post.Definition);
+      Servlet.Rest.Register (Ctx, API_Simple_Put.Definition);
+      Servlet.Rest.Register (Ctx, API_Simple_Delete.Definition);
       Ctx.Dump_Routes (Util.Log.INFO_LEVEL);
 
       Test_API_Definition.Register (Registry  => Ctx,
@@ -268,8 +268,8 @@ package body ASF.Rest.Tests is
    --  ------------------------------
    procedure Test_Create (T : in out Test) is
    begin
-      Test_Operation (T, "POST", "/test", ASF.Responses.SC_CREATED);
-      Test_Operation (T, "POST", "/simple", ASF.Responses.SC_CREATED);
+      Test_Operation (T, "POST", "/test", Servlet.Responses.SC_CREATED);
+      Test_Operation (T, "POST", "/simple", Servlet.Responses.SC_CREATED);
    end Test_Create;
 
    --  ------------------------------
@@ -277,8 +277,8 @@ package body ASF.Rest.Tests is
    --  ------------------------------
    procedure Test_Update (T : in out Test) is
    begin
-      Test_Operation (T, "PUT", "/test/44", ASF.Responses.SC_OK);
-      Test_Operation (T, "PUT", "/simple/44", ASF.Responses.SC_OK);
+      Test_Operation (T, "PUT", "/test/44", Servlet.Responses.SC_OK);
+      Test_Operation (T, "PUT", "/simple/44", Servlet.Responses.SC_OK);
    end Test_Update;
 
    --  ------------------------------
@@ -286,12 +286,12 @@ package body ASF.Rest.Tests is
    --  ------------------------------
    procedure Test_Get (T : in out Test) is
    begin
-      Test_Operation (T, "GET", "/test", ASF.Responses.SC_OK);
-      Test_Operation (T, "GET", "/test/44", ASF.Responses.SC_OK);
-      Test_Operation (T, "GET", "/test/100", ASF.Responses.SC_NOT_FOUND);
-      Test_Operation (T, "GET", "/simple", ASF.Responses.SC_OK);
-      Test_Operation (T, "GET", "/simple/44", ASF.Responses.SC_OK);
-      Test_Operation (T, "GET", "/simple/100", ASF.Responses.SC_NOT_FOUND);
+      Test_Operation (T, "GET", "/test", Servlet.Responses.SC_OK);
+      Test_Operation (T, "GET", "/test/44", Servlet.Responses.SC_OK);
+      Test_Operation (T, "GET", "/test/100", Servlet.Responses.SC_NOT_FOUND);
+      Test_Operation (T, "GET", "/simple", Servlet.Responses.SC_OK);
+      Test_Operation (T, "GET", "/simple/44", Servlet.Responses.SC_OK);
+      Test_Operation (T, "GET", "/simple/100", Servlet.Responses.SC_NOT_FOUND);
    end Test_Get;
 
    --  ------------------------------
@@ -299,8 +299,8 @@ package body ASF.Rest.Tests is
    --  ------------------------------
    procedure Test_Delete (T : in out Test) is
    begin
-      Test_Operation (T, "DELETE", "/test/44", ASF.Responses.SC_NO_CONTENT);
-      Test_Operation (T, "DELETE", "/simple/44", ASF.Responses.SC_NO_CONTENT);
+      Test_Operation (T, "DELETE", "/test/44", Servlet.Responses.SC_NO_CONTENT);
+      Test_Operation (T, "DELETE", "/simple/44", Servlet.Responses.SC_NO_CONTENT);
    end Test_Delete;
 
    --  ------------------------------
@@ -308,8 +308,8 @@ package body ASF.Rest.Tests is
    --  ------------------------------
    procedure Test_Invalid (T : in out Test) is
    begin
-      Test_Operation (T, "TRACE", "/test/44", ASF.Responses.SC_NOT_FOUND);
-      Test_Operation (T, "TRACE", "/simple/44", ASF.Responses.SC_NOT_FOUND);
+      Test_Operation (T, "TRACE", "/test/44", Servlet.Responses.SC_NOT_FOUND);
+      Test_Operation (T, "TRACE", "/simple/44", Servlet.Responses.SC_NOT_FOUND);
    end Test_Invalid;
 
-end ASF.Rest.Tests;
+end Servlet.Rest.Tests;
