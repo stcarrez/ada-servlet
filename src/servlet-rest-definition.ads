e33b996f6bfbf0c48ebf4fb523a903d466ed0330
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-04 09:26:42 +0100

    Rename package ASF into Servlet

--- src/servlet-rest-definition.ads
+++ src/servlet-rest-definition.ads
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------
---  asf-rest-definition -- REST API Definition
+--  servlet-rest-definition -- REST API Definition
 --  Copyright (C) 2016 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
@@ -18,29 +18,29 @@
 generic
    type Object_Type is limited private;
    URI : String;
-package ASF.Rest.Definition is
+package Servlet.Rest.Definition is
 
-   type Descriptor is new ASF.Rest.Descriptor with record
+   type Descriptor is new Servlet.Rest.Descriptor with record
       Handler  : access procedure (Object : in out Object_Type;
-                                   Req    : in out ASF.Rest.Request'Class;
-                                   Reply  : in out ASF.Rest.Response'Class;
-                                   Stream : in out ASF.Rest.Output_Stream'Class);
+                                   Req    : in out Servlet.Rest.Request'Class;
+                                   Reply  : in out Servlet.Rest.Response'Class;
+                                   Stream : in out Servlet.Rest.Output_Stream'Class);
    end record;
 
    overriding
    procedure Dispatch (Handler : in Descriptor;
-                       Req     : in out ASF.Rest.Request'Class;
-                       Reply   : in out ASF.Rest.Response'Class;
-                       Stream  : in out ASF.Rest.Output_Stream'Class);
+                       Req     : in out Servlet.Rest.Request'Class;
+                       Reply   : in out Servlet.Rest.Response'Class;
+                       Stream  : in out Servlet.Rest.Output_Stream'Class);
 
    --  Definition of an API operation mapped to a given URI pattern and associated with
    --  the operation handler.
    generic
       Handler    : access procedure (Object : in out Object_Type;
-                                     Req    : in out ASF.Rest.Request'Class;
-                                     Reply  : in out ASF.Rest.Response'Class;
-                                     Stream : in out ASF.Rest.Output_Stream'Class);
-      Method     : Method_Type := ASF.Rest.GET;
+                                     Req    : in out Servlet.Rest.Request'Class;
+                                     Reply  : in out Servlet.Rest.Response'Class;
+                                     Stream : in out Servlet.Rest.Output_Stream'Class);
+      Method     : Method_Type := Servlet.Rest.GET;
       Pattern    : String;
       Permission : Security.Permissions.Permission_Index := Security.Permissions.NONE;
    package Definition is
@@ -50,12 +50,12 @@ package ASF.Rest.Definition is
    --  Register the list of APIs that have been created by instantiating the <tt>Definition</tt>
    --  package.  The REST servlet identified by <tt>Name</tt> is searched in the servlet registry
    --  and used as the servlet for processing the API requests.
-   procedure Register (Registry  : in out ASF.Servlets.Servlet_Registry;
+   procedure Register (Registry  : in out Servlet.Servlets.Servlet_Registry;
                        Name      : in String;
                        ELContext : in EL.Contexts.ELContext'Class);
 
 private
 
-   Entries : ASF.Rest.Descriptor_Access;
+   Entries : Servlet.Rest.Descriptor_Access;
 
-end ASF.Rest.Definition;
+end Servlet.Rest.Definition;
