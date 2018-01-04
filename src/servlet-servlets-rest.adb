e2f6a44a2db8563c6e8432585e51d3eab273f65e
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-04 09:24:58 +0100

    Rename package ASF into Servlet

--- src/servlet-servlets-rest.adb
+++ src/servlet-servlets-rest.adb
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------
---  asf-servlets-rest -- REST servlet
+--  servlet-servlets-rest -- REST servlet
 --  Copyright (C) 2016, 2017, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
@@ -16,8 +16,8 @@
 --  limitations under the License.
 -----------------------------------------------------------------------
 
-with ASF.Streams.JSON;
-package body ASF.Servlets.Rest is
+with Servlet.Streams.JSON;
+package body Servlet.Servlets.Rest is
 
    --  ------------------------------
    --  Called by the servlet container to indicate to a servlet that the servlet
@@ -43,28 +43,28 @@ package body ASF.Servlets.Rest is
       Method : constant String := Request.Get_Method;
    begin
       if Method = "GET" then
-         Rest_Servlet'Class (Server).Dispatch (ASF.Rest.GET, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.GET, Request, Response);
 
       elsif Method = "POST" then
-         Rest_Servlet'Class (Server).Dispatch (ASF.Rest.POST, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.POST, Request, Response);
 
       elsif Method = "PUT" then
-         Rest_Servlet'Class (Server).Dispatch (ASF.Rest.PUT, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.PUT, Request, Response);
 
       elsif Method = "DELETE" then
-         Rest_Servlet'Class (Server).Dispatch (ASF.Rest.DELETE, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.DELETE, Request, Response);
 
       elsif Method = "HEAD" then
-         Rest_Servlet'Class (Server).Dispatch (ASF.Rest.POST, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.POST, Request, Response);
 
       elsif Method = "OPTIONS" then
-         Rest_Servlet'Class (Server).Dispatch (ASF.Rest.HEAD, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.HEAD, Request, Response);
 
       elsif Method = "TRACE" then
-         Rest_Servlet'Class (Server).Dispatch (ASF.Rest.TRACE, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.TRACE, Request, Response);
 
       elsif Method = "CONNECT" then
-         Rest_Servlet'Class (Server).Dispatch (ASF.Rest.CONNECT, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.CONNECT, Request, Response);
 
       else
          Response.Send_Error (Responses.SC_NOT_IMPLEMENTED);
@@ -72,64 +72,64 @@ package body ASF.Servlets.Rest is
    end Service;
 
    procedure Dispatch (Server   : in Rest_Servlet;
-                       Method   : in ASF.Rest.Method_Type;
+                       Method   : in Servlet.Rest.Method_Type;
                        Request  : in out Requests.Request'Class;
                        Response : in out Responses.Response'Class) is
       pragma Unreferenced (Server);
-      use type ASF.Routes.Route_Type_Access;
-      use type ASF.Routes.Servlets.Rest.API_Route_Type;
-      use type ASF.Rest.Descriptor_Access;
+      use type Servlet.Routes.Route_Type_Access;
+      use type Servlet.Routes.Servlets.Rest.API_Route_Type;
+      use type Servlet.Rest.Descriptor_Access;
 
-      Route  : constant ASF.Routes.Route_Type_Access := Request.Get_Route;
+      Route  : constant Servlet.Routes.Route_Type_Access := Request.Get_Route;
    begin
-      if Route = null or else not (Route.all in ASF.Routes.Servlets.Rest.API_Route_Type'Class) then
-         Response.Set_Status (ASF.Responses.SC_NOT_FOUND);
+      if Route = null or else not (Route.all in Servlet.Routes.Servlets.Rest.API_Route_Type'Class) then
+         Response.Set_Status (Servlet.Responses.SC_NOT_FOUND);
          return;
       end if;
       declare
-         Api    : constant access ASF.Routes.Servlets.Rest.API_Route_Type
-           := ASF.Routes.Servlets.Rest.API_Route_Type (Route.all)'Access;
-         Desc   : constant ASF.Rest.Descriptor_Access := Api.Descriptors (Method);
-         Output : constant ASF.Streams.Print_Stream := Response.Get_Output_Stream;
-         Stream : ASF.Streams.JSON.Print_Stream;
+         Api    : constant access Servlet.Routes.Servlets.Rest.API_Route_Type
+           := Servlet.Routes.Servlets.Rest.API_Route_Type (Route.all)'Access;
+         Desc   : constant Servlet.Rest.Descriptor_Access := Api.Descriptors (Method);
+         Output : constant Servlet.Streams.Print_Stream := Response.Get_Output_Stream;
+         Stream : Servlet.Streams.JSON.Print_Stream;
       begin
          if Desc = null then
-            Response.Set_Status (ASF.Responses.SC_NOT_FOUND);
+            Response.Set_Status (Servlet.Responses.SC_NOT_FOUND);
             return;
          end if;
 --         if not App.Has_Permission (Desc.Permission) then
---            Response.Set_Status (ASF.Responses.SC_FORBIDDEN);
+--            Response.Set_Status (Servlet.Responses.SC_FORBIDDEN);
 --            return;
 --         end if;
-         ASF.Streams.JSON.Initialize (Stream, Output);
+         Servlet.Streams.JSON.Initialize (Stream, Output);
          Response.Set_Content_Type ("application/json; charset=utf-8");
          Api.Descriptors (Method).Dispatch (Request, Response, Stream);
       end;
    end Dispatch;
 
-   function Create_Route (Registry : in ASF.Servlets.Servlet_Registry;
+   function Create_Route (Registry : in Servlet.Servlets.Servlet_Registry;
                           Name     : in String)
-                          return ASF.Routes.Servlets.Rest.API_Route_Type_Access is
+                          return Servlet.Routes.Servlets.Rest.API_Route_Type_Access is
       Pos    : constant Servlet_Maps.Cursor := Registry.Servlets.Find (Name);
-      Result : ASF.Routes.Servlets.Rest.API_Route_Type_Access;
+      Result : Servlet.Routes.Servlets.Rest.API_Route_Type_Access;
    begin
       if not Servlet_Maps.Has_Element (Pos) then
          --  Log.Error ("No servlet {0}", Name);
          raise Servlet_Error with "No servlet " & Name;
       end if;
-      Result := new ASF.Routes.Servlets.Rest.API_Route_Type;
+      Result := new Servlet.Routes.Servlets.Rest.API_Route_Type;
       Result.Servlet := Servlet_Maps.Element (Pos);
       return Result;
    end Create_Route;
 
    --  Create a route for the REST API.
-   function Create_Route (Servlet  : in ASF.Servlets.Servlet_Access)
-                          return ASF.Routes.Servlets.Rest.API_Route_Type_Access is
-      Result : ASF.Routes.Servlets.Rest.API_Route_Type_Access;
+   function Create_Route (Servlet  : in Servlet.Servlets.Servlet_Access)
+                          return Servlet.Routes.Servlets.Rest.API_Route_Type_Access is
+      Result : Servlet.Routes.Servlets.Rest.API_Route_Type_Access;
    begin
-      Result := new ASF.Routes.Servlets.Rest.API_Route_Type;
+      Result := new Servlet.Routes.Servlets.Rest.API_Route_Type;
       Result.Servlet := Servlet;
       return Result;
    end Create_Route;
 
-end ASF.Servlets.Rest;
+end Servlet.Servlets.Rest;
