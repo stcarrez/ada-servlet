1e330b8fc210ef71381c0e2620da159283f92433
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-04 11:21:01 +0100

    Rename ASF into Servlet, update for compilation

--- src/servlet-sessions.adb
+++ src/servlet-sessions.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  servlet-sessions -- Servlet Sessions
---  Copyright (C) 2010, 2011 Stephane Carrez
+--  Copyright (C) 2010, 2011, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
