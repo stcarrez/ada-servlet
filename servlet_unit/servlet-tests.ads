2dc4a6f990c1ca3552dbb6c8adf399c393dbbeb5
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-04 10:27:57 +0100

    Rename package ASF into Servlet

--- servlet_unit/servlet-tests.ads
+++ servlet_unit/servlet-tests.ads
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------
---  ASF tests - ASF Tests Framework
+--  Servlet tests - Servlet Tests Framework
 --  Copyright (C) 2011, 2012, 2015 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
@@ -17,11 +17,11 @@
 -----------------------------------------------------------------------
 
 with Util.Properties;
-with ASF.Requests.Mockup;
-with ASF.Responses.Mockup;
-with ASF.Server;
-with ASF.Servlets;
---  with ASF.Applications.Main;
+with Servlet.Requests.Mockup;
+with Servlet.Responses.Mockup;
+with Servlet.Server;
+with Servlet.Servlets;
+--  with Servlet.Applications.Main;
 with Util.Tests;
 with Util.XUnit;
 
@@ -30,48 +30,48 @@ with EL.Variables;
 
 with GNAT.Source_Info;
 
---  The <b>ASF.Tests</b> package provides a set of utility procedures to write a unit test
---  on top of ASF.
-package ASF.Tests is
+--  The <b>Servlet.Tests</b> package provides a set of utility procedures to write a unit test
+--  on top of Servlet.
+package Servlet.Tests is
 
-   --  Initialize the asf test framework mockup.  If the application is not specified,
-   --  a default ASF application is created.
+   --  Initialize the servlet-test framework mockup.  If the application is not specified,
+   --  a default Servlet application is created.
    procedure Initialize (Props       : in Util.Properties.Manager;
-                         Registry    : in ASF.Servlets.Servlet_Registry_Access := null);
---                           Application : in ASF.Applications.Main.Application_Access := null;
---                           Factory     : in out ASF.Applications.Main.Application_Factory'Class);
+                         Registry    : in Servlet.Servlets.Servlet_Registry_Access := null);
+--                           Application : in Servlet.Applications.Main.Application_Access := null;
+--                           Factory     : in out Servlet.Applications.Main.Application_Factory'Class);
 
    --  Called when the testsuite execution has finished.
    procedure Finish (Status : in Util.XUnit.Status);
 
    --  Get the server
-   function Get_Server return access ASF.Server.Container;
+   function Get_Server return access Servlet.Server.Container;
 
    --  Get the test application.
---     function Get_Application return ASF.Applications.Main.Application_Access;
+--     function Get_Application return Servlet.Applications.Main.Application_Access;
 
    --  Simulate a GET request on the given URI with the request parameters.
    --  Get the result in the response object.
-   procedure Do_Get (Request  : in out ASF.Requests.Mockup.Request;
-                     Response : in out ASF.Responses.Mockup.Response;
+   procedure Do_Get (Request  : in out Servlet.Requests.Mockup.Request;
+                     Response : in out Servlet.Responses.Mockup.Response;
                      URI      : in String;
                      Save     : in String := "");
 
    --  Simulate a POST request on the given URI with the request parameters.
    --  Get the result in the response object.
-   procedure Do_Post (Request  : in out ASF.Requests.Mockup.Request;
-                      Response : in out ASF.Responses.Mockup.Response;
+   procedure Do_Post (Request  : in out Servlet.Requests.Mockup.Request;
+                      Response : in out Servlet.Responses.Mockup.Response;
                       URI      : in String;
                       Save     : in String := "");
 
    --  Simulate a raw request.  The URI and method must have been set on the Request object.
-   procedure Do_Req (Request  : in out ASF.Requests.Mockup.Request;
-                     Response : in out ASF.Responses.Mockup.Response);
+   procedure Do_Req (Request  : in out Servlet.Requests.Mockup.Request;
+                     Response : in out Servlet.Responses.Mockup.Response);
 
    --  Check that the response body contains the string
    procedure Assert_Contains (T       : in Util.Tests.Test'Class;
                               Value   : in String;
-                              Reply   : in out ASF.Responses.Mockup.Response;
+                              Reply   : in out Servlet.Responses.Mockup.Response;
                               Message : in String := "Test failed";
                               Source  : String := GNAT.Source_Info.File;
                               Line    : Natural := GNAT.Source_Info.Line);
@@ -79,16 +79,16 @@ package ASF.Tests is
    --  Check that the response body matches the regular expression
    procedure Assert_Matches (T       : in Util.Tests.Test'Class;
                              Pattern : in String;
-                             Reply   : in out ASF.Responses.Mockup.Response;
+                             Reply   : in out Servlet.Responses.Mockup.Response;
                              Message : in String := "Test failed";
-                             Status  : in Natural := ASF.Responses.SC_OK;
+                             Status  : in Natural := Servlet.Responses.SC_OK;
                              Source  : String := GNAT.Source_Info.File;
                              Line    : Natural := GNAT.Source_Info.Line);
 
    --  Check that the response body is a redirect to the given URI.
    procedure Assert_Redirect (T       : in Util.Tests.Test'Class;
                               Value   : in String;
-                              Reply   : in out ASF.Responses.Mockup.Response;
+                              Reply   : in out Servlet.Responses.Mockup.Response;
                               Message : in String := "Test failed";
                               Source  : String := GNAT.Source_Info.File;
                               Line    : Natural := GNAT.Source_Info.Line);
@@ -97,9 +97,9 @@ package ASF.Tests is
    procedure Assert_Header (T       : in Util.Tests.Test'Class;
                             Header  : in String;
                             Value   : in String;
-                            Reply   : in out ASF.Responses.Mockup.Response;
+                            Reply   : in out Servlet.Responses.Mockup.Response;
                             Message : in String := "Test failed";
-                            Status  : in Natural := ASF.Responses.SC_OK;
+                            Status  : in Natural := Servlet.Responses.SC_OK;
                             Source  : String := GNAT.Source_Info.File;
                             Line    : Natural := GNAT.Source_Info.Line);
 
@@ -122,4 +122,4 @@ package ASF.Tests is
    overriding
    procedure Set_Up (T : in out EL_Test);
 
-end ASF.Tests;
+end Servlet.Tests;
