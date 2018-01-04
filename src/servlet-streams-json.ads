90239c019fea5be7b8889f6cb78eab0310e4059a
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-04 09:22:37 +0100

    Rename package ASF into Servlet

--- src/servlet-streams-json.ads
+++ src/servlet-streams-json.ads
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------
---  asf-streams-json -- JSON Print streams for servlets
+--  servlet-streams-json -- JSON Print streams for servlets
 --  Copyright (C) 2016 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
@@ -17,12 +17,12 @@
 -----------------------------------------------------------------------
 
 with Util.Serialize.IO.JSON;
-package ASF.Streams.JSON is
+package Servlet.Streams.JSON is
 
    subtype Print_Stream is Util.Serialize.IO.JSON.Output_Stream;
 
    --  Initialize the stream
    procedure Initialize (Stream : in out Print_Stream;
-                         To     : in ASF.Streams.Print_Stream'Class);
+                         To     : in Servlet.Streams.Print_Stream'Class);
 
-end ASF.Streams.JSON;
+end Servlet.Streams.JSON;
