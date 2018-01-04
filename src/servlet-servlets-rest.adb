b22b70dd6c7cb4e6cb602203eff2f6e396fb2e68
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-04 11:21:53 +0100

    Rename ASF into Servlet, update for compilation

--- src/servlet-servlets-rest.adb
+++ src/servlet-servlets-rest.adb
@@ -43,28 +43,28 @@ package body Servlet.Servlets.Rest is
       Method : constant String := Request.Get_Method;
    begin
       if Method = "GET" then
-         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.GET, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (GET, Request, Response);
 
       elsif Method = "POST" then
-         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.POST, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (POST, Request, Response);
 
       elsif Method = "PUT" then
-         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.PUT, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (PUT, Request, Response);
 
       elsif Method = "DELETE" then
-         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.DELETE, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (DELETE, Request, Response);
 
       elsif Method = "HEAD" then
-         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.POST, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (POST, Request, Response);
 
       elsif Method = "OPTIONS" then
-         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.HEAD, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (HEAD, Request, Response);
 
       elsif Method = "TRACE" then
-         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.TRACE, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (TRACE, Request, Response);
 
       elsif Method = "CONNECT" then
-         Rest_Servlet'Class (Server).Dispatch (Servlet.Rest.CONNECT, Request, Response);
+         Rest_Servlet'Class (Server).Dispatch (CONNECT, Request, Response);
 
       else
          Response.Send_Error (Responses.SC_NOT_IMPLEMENTED);
@@ -72,62 +72,60 @@ package body Servlet.Servlets.Rest is
    end Service;
 
    procedure Dispatch (Server   : in Rest_Servlet;
-                       Method   : in Servlet.Rest.Method_Type;
+                       Method   : in Method_Type;
                        Request  : in out Requests.Request'Class;
                        Response : in out Responses.Response'Class) is
       pragma Unreferenced (Server);
-      use type Servlet.Routes.Route_Type_Access;
-      use type Servlet.Routes.Servlets.Rest.API_Route_Type;
-      use type Servlet.Rest.Descriptor_Access;
+      use type Routes.Servlets.Rest.API_Route_Type;
 
-      Route  : constant Servlet.Routes.Route_Type_Access := Request.Get_Route;
+      Route  : constant Routes.Route_Type_Access := Request.Get_Route;
    begin
-      if Route = null or else not (Route.all in Servlet.Routes.Servlets.Rest.API_Route_Type'Class) then
-         Response.Set_Status (Servlet.Responses.SC_NOT_FOUND);
+      if Route = null or else not (Route.all in Routes.Servlets.Rest.API_Route_Type'Class) then
+         Response.Set_Status (Responses.SC_NOT_FOUND);
          return;
       end if;
       declare
-         Api    : constant access Servlet.Routes.Servlets.Rest.API_Route_Type
-           := Servlet.Routes.Servlets.Rest.API_Route_Type (Route.all)'Access;
-         Desc   : constant Servlet.Rest.Descriptor_Access := Api.Descriptors (Method);
-         Output : constant Servlet.Streams.Print_Stream := Response.Get_Output_Stream;
-         Stream : Servlet.Streams.JSON.Print_Stream;
+         Api    : constant access Routes.Servlets.Rest.API_Route_Type
+           := Routes.Servlets.Rest.API_Route_Type (Route.all)'Access;
+         Desc   : constant Descriptor_Access := Api.Descriptors (Method);
+         Output : constant Streams.Print_Stream := Response.Get_Output_Stream;
+         Stream : Streams.JSON.Print_Stream;
       begin
          if Desc = null then
-            Response.Set_Status (Servlet.Responses.SC_NOT_FOUND);
+            Response.Set_Status (Responses.SC_NOT_FOUND);
             return;
          end if;
 --         if not App.Has_Permission (Desc.Permission) then
---            Response.Set_Status (Servlet.Responses.SC_FORBIDDEN);
+--            Response.Set_Status (Responses.SC_FORBIDDEN);
 --            return;
 --         end if;
-         Servlet.Streams.JSON.Initialize (Stream, Output);
+         Streams.JSON.Initialize (Stream, Output);
          Response.Set_Content_Type ("application/json; charset=utf-8");
          Api.Descriptors (Method).Dispatch (Request, Response, Stream);
       end;
    end Dispatch;
 
-   function Create_Route (Registry : in Servlet.Servlets.Servlet_Registry;
+   function Create_Route (Registry : in Servlets.Servlet_Registry;
                           Name     : in String)
-                          return Servlet.Routes.Servlets.Rest.API_Route_Type_Access is
+                          return Routes.Servlets.Rest.API_Route_Type_Access is
       Pos    : constant Servlet_Maps.Cursor := Registry.Servlets.Find (Name);
-      Result : Servlet.Routes.Servlets.Rest.API_Route_Type_Access;
+      Result : Routes.Servlets.Rest.API_Route_Type_Access;
    begin
       if not Servlet_Maps.Has_Element (Pos) then
          --  Log.Error ("No servlet {0}", Name);
          raise Servlet_Error with "No servlet " & Name;
       end if;
-      Result := new Servlet.Routes.Servlets.Rest.API_Route_Type;
+      Result := new Routes.Servlets.Rest.API_Route_Type;
       Result.Servlet := Servlet_Maps.Element (Pos);
       return Result;
    end Create_Route;
 
    --  Create a route for the REST API.
-   function Create_Route (Servlet  : in Servlet.Servlets.Servlet_Access)
-                          return Servlet.Routes.Servlets.Rest.API_Route_Type_Access is
-      Result : Servlet.Routes.Servlets.Rest.API_Route_Type_Access;
+   function Create_Route (Servlet  : in Servlets.Servlet_Access)
+                          return Routes.Servlets.Rest.API_Route_Type_Access is
+      Result : Routes.Servlets.Rest.API_Route_Type_Access;
    begin
-      Result := new Servlet.Routes.Servlets.Rest.API_Route_Type;
+      Result := new Routes.Servlets.Rest.API_Route_Type;
       Result.Servlet := Servlet;
       return Result;
    end Create_Route;
