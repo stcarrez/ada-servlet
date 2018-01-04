bef86615da3e23218030f8439d3c10acca2f4ed0
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-04 11:25:15 +0100

    Rename ASF into Servlet, update for compilation

--- src/servlet-servlets-files.ads
+++ src/servlet-servlets-files.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  servlet-servlets.files -- Static file servlet
---  Copyright (C) 2010, 2011 Stephane Carrez
+--  Copyright (C) 2010, 2011, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
