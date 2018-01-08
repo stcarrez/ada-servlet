2a97cef83bb6fe7fb45ad7b48538d77fa1c0db1f
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-08 21:21:11 +0100

    Rename ASF into Servlet

--- samples/api_server.adb
+++ samples/api_server.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  api_server -- Example of REST API server
---  Copyright (C) 2016 Stephane Carrez
+--  Copyright (C) 2016, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,11 +16,10 @@
 --  limitations under the License.
 -----------------------------------------------------------------------
 
-with ASF.Server.Web;
-with ASF.Servlets.Rest;
-with ASF.Servlets.Files;
-with ASF.Applications;
-with ASF.Rest;
+with Servlet.Server.Web;
+with Servlet.Servlets.Rest;
+with Servlet.Servlets.Files;
+with Servlet.Rest;
 with Util.Log.Loggers;
 with Monitor;
 with EL.Contexts.Default;
@@ -28,15 +27,15 @@ with EL.Contexts.Default;
 procedure API_Server is
    CONFIG_PATH  : constant String := "samples.properties";
 
-   Api     : aliased ASF.Servlets.Rest.Rest_Servlet;
-   Files   : aliased ASF.Servlets.Files.File_Servlet;
-   App     : aliased ASF.Servlets.Servlet_Registry;
-   WS      : ASF.Server.Web.AWS_Container;
+   Api     : aliased Servlet.Servlets.Rest.Rest_Servlet;
+   Files   : aliased Servlet.Servlets.Files.File_Servlet;
+   App     : aliased Servlet.Servlets.Servlet_Registry;
+   WS      : Servlet.Server.Web.AWS_Container;
    Ctx     : EL.Contexts.Default.Default_Context;
    Log     : constant Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("Api_Server");
 begin
    Util.Log.Loggers.Initialize (CONFIG_PATH);
-   App.Set_Init_Parameter (ASF.Applications.VIEW_DIR, "samples/web/monitor");
+--   App.Set_Init_Parameter (ASF.Applications.VIEW_DIR, "samples/web/monitor");
 
    --  Register the servlets and filters
    App.Add_Servlet (Name => "api", Server => Api'Unchecked_Access);
@@ -49,8 +48,8 @@ begin
    App.Add_Mapping (Name => "files", Pattern => "*.js");
 
 --   Monitor.Mon_API.Register (App, "api", Ctx);
-   ASF.Rest.Register (App, Monitor.API_Get_Values.Definition);
-   ASF.Rest.Register (App, Monitor.API_Put_Value.Definition);
+   Servlet.Rest.Register (App, Monitor.API_Get_Values.Definition);
+   Servlet.Rest.Register (App, Monitor.API_Put_Value.Definition);
 
    WS.Register_Application ("/monitor", App'Unchecked_Access);
 
