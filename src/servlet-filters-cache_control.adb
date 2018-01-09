2437dcf4e45d20a64a6a98a1f22b0697b863a058
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-09 21:01:29 +0100

    Rename Servlet.Servlets into Servlet.Core

--- src/servlet-filters-cache_control.adb
+++ src/servlet-filters-cache_control.adb
@@ -29,7 +29,7 @@ package body Servlet.Filters.Cache_Control is
    procedure Do_Filter (F        : in Cache_Control_Filter;
                         Request  : in out Requests.Request'Class;
                         Response : in out Responses.Response'Class;
-                        Chain    : in out Servlet.Servlets.Filter_Chain) is
+                        Chain    : in out Servlet.Core.Filter_Chain) is
       use Ada.Strings.Unbounded;
    begin
       if Length (F.Cache_Control_Header) > 0 then
@@ -38,7 +38,7 @@ package body Servlet.Filters.Cache_Control is
       if Length (F.Vary) > 0 then
          Response.Add_Header ("Vary", To_String (F.Vary));
       end if;
-      Servlet.Servlets.Do_Filter (Chain    => Chain,
+      Servlet.Core.Do_Filter (Chain    => Chain,
                               Request  => Request,
                               Response => Response);
    end Do_Filter;
@@ -48,10 +48,10 @@ package body Servlet.Filters.Cache_Control is
    --  instance is being placed into service.
    --  ------------------------------
    procedure Initialize (Server  : in out Cache_Control_Filter;
-                         Config  : in Servlet.Servlets.Filter_Config) is
+                         Config  : in Servlet.Core.Filter_Config) is
    begin
-      Server.Vary                 := Servlets.Get_Init_Parameter (Config, VARY_HEADER_PARAM);
-      Server.Cache_Control_Header := Servlets.Get_Init_Parameter (Config, CACHE_CONTROL_PARAM);
+      Server.Vary                 := Core.Get_Init_Parameter (Config, VARY_HEADER_PARAM);
+      Server.Cache_Control_Header := Core.Get_Init_Parameter (Config, CACHE_CONTROL_PARAM);
    end Initialize;
 
 end Servlet.Filters.Cache_Control;
