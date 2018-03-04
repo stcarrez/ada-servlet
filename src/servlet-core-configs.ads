72d87e62cd7e4b186c065c7635987599f34ee49d
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-03-04 19:31:07 +0100

    New package Servlet.Core.Configs with Read_Configuration procedure

---
+++ src/servlet-core-configs.ads
@@ -0,0 +1,25 @@
+-----------------------------------------------------------------------
+--  servlet-core-configs -- Read servlet configuration files
+--  Copyright (C) 2018 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+package Servlet.Core.Configs is
+
+   --  Read the configuration file associated with the servlet container.
+   procedure Read_Configuration (App  : in out Servlet_Registry'Class;
+                                 File : in String);
+
+end Servlet.Core.Configs;
