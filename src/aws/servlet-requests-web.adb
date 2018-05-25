6d405634ee4c6b729f229a1d6b9fb35ffc4ed12d
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-05-25 23:01:17 +0200

    Add overriding for operations that override

--- src/aws/servlet-requests-web.adb
+++ src/aws/servlet-requests-web.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  servlet-requests -- Servlet Requests
---  Copyright (C) 2009, 2010, 2011, 2012, 2013, 2015, 2017 Stephane Carrez
+--  Copyright (C) 2009, 2010, 2011, 2012, 2013, 2015, 2017, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
