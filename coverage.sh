a1219dba3a427e1fc9c52b839e59e2de423f61fd
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-12 23:30:40 +0100

    Update and cleanup coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-lcov --base-directory . --directory . -c -o servlet.cov >/dev/null
-bin/servlet_harness -t 120 -xml servlet-aunit.xml -config test.properties
-(lcov --base-directory . --directory . -c -o servlet.cov
-lcov --remove servlet.cov "/usr*" -o servlet.cov 
-lcov --remove servlet.cov "/opt*" -o servlet.cov 
-lcov --remove servlet.cov "regtests*" -o servlet.cov
-lcov --remove servlet.cov "*/ada-util/*" -o servlet.cov
-lcov --remove servlet.cov "*/b__servlet_harness.adb" -o servlet.cov ) >/dev/null
+NAME=servletada.cov
+lcov --base-directory . --directory . -c --include "*/ada-servlet/src/*" -o $NAME
 rm -rf cover
-genhtml -o ./cover -t "test coverage" --num-spaces 4 servlet.cov
+genhtml --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
