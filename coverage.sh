f25f59f732768f1722641407eae78deeec4dbb9c
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-14 23:39:59 +0100

    Add --quiet for coverage

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 NAME=servletada.cov
-lcov --base-directory . --directory . -c --include "*/ada-servlet/src/*" -o $NAME
+lcov --quiet --base-directory . --directory . -c --include "*/ada-servlet/src/*" -o $NAME
 rm -rf cover
-genhtml --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
+genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
